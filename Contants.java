ced3a4b2f7e7be03c702bc81c519cf2d87caf07d
likaixun <18601245439@163.com>
2017-04-08 11:04:48 +0800

    test

---
+++ Contants.java
@@ -0,0 +1,15 @@
+package utility;
+
+public class Contants {
+	
+	public static final int keycolumn =0;
+	public static final int column =1;
+	public static final String path = System.getProperty("user.dir")+ "\\src\\testdata\\";
+	public static final String filename = "Exceldata.xlsx";
+	public static final String csvFileName = "Exceldata.csv";
+	public static final String sheetname = "Login";
+	
+	
+	
+
+}
